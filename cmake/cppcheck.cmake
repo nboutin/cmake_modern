f666b013ffe7dba612e0bec32b5fab4b73b9ec3d
nboutin <boutwork@gmail.com>
2018-11-05 22:30:50 +0100

    Fix cppcheck.cmake missing parenthesis

--- cmake/cppcheck.cmake
+++ cmake/cppcheck.cmake
@@ -25,7 +25,8 @@ if(ENABLE_CPPCHECK)
 	    --language=c++
 	    -DMAIN=main
 	    -I ${CMAKE_SOURCE_DIR}/source_bin/*
-#	    -I ${CMAKE_SOURCE_DIR}/test/src/*)
+#	    -I ${CMAKE_SOURCE_DIR}/test/src/*
+)
 
 	add_custom_target(cppcheck COMMAND ${CMAKE_BINARY_DIR}/bin/cppcheck ${CPPCHECK_ARGS})
 endif()
